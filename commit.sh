eabb88017f7f823255f66217bd424bdee49022bb
Mikkel Paulson <git@email.mikkel.ca>
2021-12-09 14:57:22 -0500

    create a new release on each push

--- commit.sh
+++ commit.sh
@@ -9,7 +9,13 @@ git pull
 
 git add images
 
-sed -i "/^This is a collection/c This is a collection of $(find images -type f | wc -l) portraits scraped from artflow.ai." README.md
+image_count="$(find images -type f | wc -l)"
+new_image_count="$(git status --porcelain=v1 | grep -c '^A  images')"
+date="$(date +%Y-%m-%d)"
+datever="$(date +%Y.%m.%d)"
+size="$(du -sb --si images | awk '{ print $1; }' | sed 's/[A-Za-z]*$/ \0B/')"
+
+sed -i "/^This is a collection/c This is a collection of $image_count portraits scraped from artflow.ai." README.md
 
 git add README.md
 
@@ -18,6 +24,10 @@ git \
   -c user.email="$USER_EMAIL" \
   -c user.signingkey="$USER_SIGNINGKEY" \
 commit \
-  -m "$(date +%Y-%m-%d): add $(git status --porcelain=v1 | grep -c '^A  images') images"
+  -m "$date: add $new_image_count images"
 
 git push
+
+if command -v gh && [ -v GITHUB_TOKEN ]; then
+  gh release -R initiative-sh/artflow-portraits create --notes "$image_count portraits as of $date ($size)" "$datever"
+fi
