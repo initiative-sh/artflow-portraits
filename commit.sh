c874f6cb2a229cf9b1a2165e318d539f27cd5a60
Mikkel Paulson <git@email.mikkel.ca>
2022-01-10 10:51:33 -0500

    update commit script to skip with < 100 images
    The input has slowed to a trickle for reasons out of my control, so
    updated the commit script to only commit if 100 or more new images have
    been collected.

--- commit.sh
+++ commit.sh
@@ -8,27 +8,31 @@ source .env
 
 git pull
 
-git add images
+if [[ "$(git status --porcelain=v1 | grep -c '^\(??\|A \) images')" -lt 100 ]]; then
+  git add images
 
-image_count="$(printf "%'d" "$(find images -type f | wc -l)")"
-new_image_count="$(printf "%'d" "$(git status --porcelain=v1 | grep -c '^A  images')")"
-date="$(date +%Y-%m-%d)"
-datever="$(date +%Y.%m.%d)"
-size="$(du -sb --si images | awk '{ print $1; }' | sed 's/[A-Za-z]*$/ \0B/')"
+  image_count_pretty="$(printf "%'d" "$(find images -type f | wc -l)")"
+  new_image_count_pretty="$(printf "%'d" "$(git status --porcelain=v1 | grep -c '^A  images')")"
+  date_pretty="$(date '+%B %-d, %y')"
+  datever="$(date +%Y.%m.%d)"
+  size="$(du -sb --si images | awk '{ print $1; }' | sed 's/[A-Za-z]*$/ \0B/')"
 
-sed -i "/^This is a collection/c This is a collection of $image_count portraits scraped from artflow.ai." README.md
+  sed -i "/^This is a collection/c This is a collection of $image_count_pretty portraits scraped from artflow.ai." README.md
 
-git add README.md
+  git add README.md
 
-git \
-  -c user.name="$USER_NAME"\
-  -c user.email="$USER_EMAIL" \
-  -c user.signingkey="$USER_SIGNINGKEY" \
-commit \
-  -m "$date: add $new_image_count images"
+  git \
+    -c user.name="$USER_NAME"\
+    -c user.email="$USER_EMAIL" \
+    -c user.signingkey="$USER_SIGNINGKEY" \
+  commit \
+    -m "automatic commit: add $new_image_count_pretty images"
 
-git push
+  git push
+else
+  echo "Found fewer than 100 new images; skipping commit for today."
+fi
 
 if [[ -v GITHUB_TOKEN && "$(date +%w)" -eq 1 ]] && command -v gh; then
-  env GITHUB_TOKEN="$GITHUB_TOKEN" gh release -R initiative-sh/artflow-portraits create --notes "$image_count portraits as of $date ($size)" "$datever"
+  env GITHUB_TOKEN="$GITHUB_TOKEN" gh release -R initiative-sh/artflow-portraits create --notes "A collection of $image_count_pretty CC-BY portraits from artflow.ai collected on $date_pretty ($size uncompressed)." "$datever"
 fi
